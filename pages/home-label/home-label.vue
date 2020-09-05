71347d9ebe10e96c2b52130e63bcda8258eb84bf
di <xd@Obj.com>
2020-09-05 22:00:50 +0800

    新建标签页管理页面

---
+++ pages/home-label/home-label.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="home-lebel">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+
+</style>
